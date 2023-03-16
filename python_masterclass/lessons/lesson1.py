42ce076fa41f111c43f51dec288d3048a436827e
Alphonse-Brandon <njei.afuh@batazia.com>
2023-03-16 15:56:00 +0100

    First session updates

---
+++ python_masterclass/lessons/lesson1.py
@@ -0,0 +1,20 @@
+# Declare an integer variable
+
+
+# Declare a float variable
+
+
+# Declare a string variable
+
+
+# Declare a boolean variable
+
+
+# Convert a string to an integer
+
+
+# Convert a float to an integer
+
+
+# Convert an integer to a string
+
